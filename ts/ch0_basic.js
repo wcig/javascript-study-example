80b70fa5ac00a43cb6a29c69d8bea9e439384eb2
wcig <job.yangbo@gmail.com>
2022-09-12 12:00:18 +0800

    doc: ts: ch0: 基础语法

---
+++ ts/ch0_basic.js
@@ -0,0 +1,7 @@
+var log = console.log.bind(console);
+// ch1: 基础语法
+// 1.与JavaScript相同处
+// - 注释
+// - 运算符
+// - 条件语句
+// - 循环语句
---
+++ ts/ch0_basic.ts
@@ -0,0 +1,9 @@
+const log = console.log.bind(console);
+
+// ch1: 基础语法
+
+// 1.与JavaScript相同处
+// - 注释
+// - 运算符
+// - 条件语句
+// - 循环语句
