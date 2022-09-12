064e15250924a0b09adcbf19a76d5ecfdd71f38c
wcig <job.yangbo@gmail.com>
2022-09-12 17:28:16 +0800

    doc: ts: ch12: 迭代器和生成器

---
+++ ts/ch12_iterator.js
@@ -0,0 +1,29 @@
+// ch12: 迭代器和生成器
+// 1.迭代器
+{
+    // 1) 对象实现了Symbol.iterator属性表示它是可迭代的, 即可使用for...of语句
+    var list = [1, 10, 100];
+    for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+        var v = list_1[_i];
+        console.log(v);
+    }
+    // 2) for...in也可以 用于迭代, 但主要用于对象键的遍历
+    for (var i in list) {
+        console.log(i, list[i]);
+    }
+}
+// 2.生成器
+{
+    // 1) 当生成目标位ES5,ES3时, 迭代器只允许在Array类型使用, 在非数组使用for...of会报错, 尽管这些非数组值已经实现了Symbol.iterator属性.
+    var arr = [1, 2, 3];
+    for (var _a = 0, arr_1 = arr; _a < arr_1.length; _a++) {
+        var v = arr_1[_a];
+        console.log(v);
+    }
+    // 生成代码
+    // var arr = [1, 2, 3];
+    // for (var _a = 0, arr_1 = arr; _a < arr_1.length; _a++) {
+    //     var v = arr_1[_a];
+    //     console.log(v);
+    // }
+}
