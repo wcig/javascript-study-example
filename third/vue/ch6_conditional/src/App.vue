640ec6a27e988995dd36313f0ae13ad256628a68
wcig <job.yangbo@gmail.com>
2022-10-04 12:22:50 +0800

    add: third: vue: ch6 条件渲染

---
+++ third/vue/ch6_conditional/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+// 1.条件绑定
+// 1) v-if, v-else, v-else-if
+const awesome = ref(true)
+
+const type = ref('D')
+
+// 2) v-show (修改的display属性)
+const show = ref(true)
+</script>
+
+<template>
+  <button @click="awesome = !awesome">Toggle</button>
+  <h1 v-if="awesome">Vue is awesome!</h1>
+  <h1 v-else>Oh no</h1>
+
+  <div v-if="type === 'A'">
+    A
+  </div>
+  <div v-else-if="type === 'B'">
+    B
+  </div>
+  <div v-else-if="type === 'C'">
+    C
+  </div>
+  <div v-else>
+    Not A/B/C
+  </div>
+
+  <div v-show="show">Hello!</div>
+</template>
