016e7eda011acacb27cc1fe0cac7085c5ca10fea
wcig <job.yangbo@gmail.com>
2022-10-05 23:27:29 +0800

    add: third: vue: ch30 应用规模化-状态管理

---
+++ third/vue/ch30_state_management/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+import ComponentA from "./ComponentA.vue"
+import ComponentB from "./ComponentB.vue"
+
+// 状态
+const count = ref(0)
+// 动作
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <!--视图-->
+  <button @click="increment">Add</button>
+  <p>{{ count }}</p>
+  <br/>
+
+  <ComponentA/>
+  <ComponentB/>
+</template>
---
+++ third/vue/ch30_state_management/src/ComponentB.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { store } from './store.js'
+</script>
+
+<template>
+  <div>
+    <button @click="store.increment()">
+      From B: {{ store.count }}
+    </button>
+  </div>
+</template>
