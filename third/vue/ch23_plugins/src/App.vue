5bf886fa4ba2cba916fecc2eed3cf7335ba926b8
wcig <job.yangbo@gmail.com>
2022-10-05 18:36:53 +0800

    add: third: vue: ch23 逻辑复用-插件

---
+++ third/vue/ch23_plugins/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import i18nPlugin from './i18n.js'
+import { createApp } from "vue"
+
+const app = createApp({})
+app.use(i18nPlugin, {
+  greetings: {
+    hello: 'Bonjour!'
+  }
+})
+</script>
+
+<template>
+  <!--App.vue:14 Uncaught TypeError: _ctx.$translate is not a function-->
+  <h1>{{ $translate('greetings.hello') }}</h1>
+</template>
