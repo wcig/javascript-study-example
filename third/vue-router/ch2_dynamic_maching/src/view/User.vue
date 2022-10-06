d5eb09b90aa5bbe561c89532d9f14a9fa233df4f
wcig <job.yangbo@gmail.com>
2022-10-06 17:41:27 +0800

    add: third: vue-router: ch2 动态路由匹配

---
+++ third/vue-router/ch2_dynamic_maching/src/view/User.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>User {{ $route.params.id }}</div>
+  <div>User {{ id }}</div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+// $route信息
+const route = useRoute()
+console.log(route)
+console.log(route.params.id)
+
+const id = ref(route.params.id)
+</script>
