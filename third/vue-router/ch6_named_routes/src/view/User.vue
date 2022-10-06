f0145288b4cc2e63f612165ce68be6438abed248
wcig <job.yangbo@gmail.com>
2022-10-06 20:17:46 +0800

    add: third: vue-router: ch6 命名路由

---
+++ third/vue-router/ch6_named_routes/src/view/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>User {{ username }}</div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+// $route信息
+const route = useRoute()
+console.log(route)
+console.log(route.params)
+
+const username = ref(route.params.username)
+</script>
