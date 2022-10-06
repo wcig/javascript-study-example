c2f311f69e51deb3c8d0ea99f7ff425157cabb91
wcig <job.yangbo@gmail.com>
2022-10-06 20:57:32 +0800

    add: third: vue-router: ch9 路由组件传参

---
+++ third/vue-router/ch9_passing_props/src/router/index.js
@@ -0,0 +1,47 @@
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import Home from '../view/Home.vue'
+import About from '../view/About.vue'
+import User from '../view/User.vue'
+import NotFound from '../view/NotFound.vue'
+
+// 1. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+    {
+        path: '/',
+        redirect: 'home'
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'about',
+        component: About
+    },
+    {
+        path: '/user/:username',
+        name: 'user',
+        component: User,
+        props: true
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFound
+    },
+]
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHashHistory(),
+    routes: routes, // `routes: routes` 的缩写
+})
+
+export default router
---
+++ third/vue-router/ch9_passing_props/src/view/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>User Name {{ username }}</div>
+  <div>User Name {{ props.username }}</div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+// 获取参数方式一: useRoute
+const route = useRoute()
+console.log(route.params)
+const username = ref(route.params.username)
+
+// 获取参数方式二: props
+const props = defineProps(['username'])
+console.log(props)
+</script>
