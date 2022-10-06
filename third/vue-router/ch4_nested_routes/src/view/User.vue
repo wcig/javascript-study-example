7b98c2c221e0b720b7e9f9eaf0a62662072d88ac
wcig <job.yangbo@gmail.com>
2022-10-06 18:39:48 +0800

    add: third: vue-router: ch4 嵌套路由

---
+++ third/vue-router/ch4_nested_routes/src/view/User.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>User {{ id }}</div>
+  <p><router-link to="/user/123">User Home</router-link> |
+    <router-link to="/user/123/profile">User Profile</router-link> |
+    <router-link to="/user/123/posts">User Posts</router-link>
+  </p>
+  <router-view></router-view>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+// $route信息
+const route = useRoute()
+console.log(route)
+console.log(route.params.id)
+
+const id = ref(route.params.id)
+</script>
---
+++ third/vue-router/ch4_nested_routes/src/view/UserHome.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>User Home</div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log(route.params)
+</script>
