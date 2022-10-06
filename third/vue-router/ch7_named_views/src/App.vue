f922e20cc120262bf6fb9c3af3a63af60b9ea7d2
wcig <job.yangbo@gmail.com>
2022-10-06 20:36:34 +0800

    add: third: vue-router: ch7 命名视图

---
+++ third/vue-router/ch7_named_views/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Hello App!</h1>
+  <p>
+    <!--使用 router-link 组件进行导航 -->
+    <!--通过传递 `to` 来指定链接 -->
+    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+    <router-link to="/">Go to Home</router-link> |
+    <router-link to="/about">Go to About</router-link> |
+    <router-link to="/other">Go to Other</router-link>
+  </p>
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <router-view name="Header"></router-view>
+  <router-view></router-view>
+  <router-view name="Footer"></router-view>
+</template>
---
+++ third/vue-router/ch7_named_views/src/view/Other.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Main</p>
+</template>
