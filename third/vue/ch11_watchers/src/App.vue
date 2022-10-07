49d5d07e36dbac3268b9cf6fe4ca2add76ad930c
wcig <job.yangbo@gmail.com>
2022-10-07 21:52:20 +0800

    add: third: vue: ch11 侦听器

--- third/vue/ch11_watchers/src/App.vue
+++ third/vue/ch11_watchers/src/App.vue
@@ -6,15 +6,18 @@ const answer = ref('Questions usually contain a question mark. ;-)')
 
 // 可以直接侦听一个 ref
 watch(question, async (newQuestion, oldQuestion) => {
-  if (newQuestion.indexOf('?') > -1) {
-    answer.value = 'Thinking...'
-    try {
-      const res = await fetch('https://yesno.wtf/api')
-      answer.value = (await res.json()).answer
-    } catch (error) {
-      answer.value = 'Error! Could not reach the API. ' + error
-    }
-  }
+  console.log('old:', oldQuestion)
+  console.log('new:', newQuestion)
+
+  // if (newQuestion.indexOf('?') > -1) {
+  //   answer.value = 'Thinking...'
+  //   try {
+  //     const res = await fetch('https://yesno.wtf/api')
+  //     answer.value = (await res.json()).answer
+  //   } catch (error) {
+  //     answer.value = 'Error! Could not reach the API. ' + error
+  //   }
+  // }
 })
 </script>
 
