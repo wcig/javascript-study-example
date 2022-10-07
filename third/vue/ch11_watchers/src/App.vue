0d97b6388c07844566415eb281c0f3132fb3541d
wcig <job.yangbo@gmail.com>
2022-10-07 21:52:20 +0800

    add: third: vue: ch11 侦听器

--- third/vue/ch11_watchers/src/App.vue
+++ third/vue/ch11_watchers/src/App.vue
@@ -1,20 +1,32 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, reactive } from 'vue'
 
 const question = ref('')
 const answer = ref('Questions usually contain a question mark. ;-)')
+const user = reactive({name: 'tom'})
 
 // 可以直接侦听一个 ref
 watch(question, async (newQuestion, oldQuestion) => {
-  if (newQuestion.indexOf('?') > -1) {
-    answer.value = 'Thinking...'
-    try {
-      const res = await fetch('https://yesno.wtf/api')
-      answer.value = (await res.json()).answer
-    } catch (error) {
-      answer.value = 'Error! Could not reach the API. ' + error
-    }
-  }
+  console.log('old:', oldQuestion)
+  console.log('new:', newQuestion)
+
+  // if (newQuestion.indexOf('?') > -1) {
+  //   answer.value = 'Thinking...'
+  //   try {
+  //     const res = await fetch('https://yesno.wtf/api')
+  //     answer.value = (await res.json()).answer
+  //   } catch (error) {
+  //     answer.value = 'Error! Could not reach the API. ' + error
+  //   }
+  // }
+})
+
+// 深层监听器: 监听对象支持深层修改, 但此时新旧值为同一个对象, 获取不到旧的值.
+watch(user, (newValue, oldValue) => {
+  console.log(newValue === oldValue) // true
+  const newUserName = newValue.name
+  const oldUserName = oldValue.name
+  console.log(newUserName, oldUserName) // jerry jerry
 })
 </script>
 
@@ -24,4 +36,8 @@ watch(question, async (newQuestion, oldQuestion) => {
     <input v-model="question" />
   </p>
   <p>{{ answer }}</p>
+  <br/>
+
+  <p>{{ user.name }}</p>
+  <button @click="user.name='jerry'">modify-username</button>
 </template>
